c6bf12573e4bf27dd13ad89227cc76f260d8f95f
Ben Greenawald <bgreenawald@gmail.com>
2018-11-16 14:34:25 -0500

    Added touch functionality and Sync powershell script

---
+++ Bash-Tools.ps1
@@ -0,0 +1,15 @@
+﻿# From https://medium.com/powershell-explained/text-editor-inside-powershell-22d2f5e748b8
+function vim($File){
+    $File = $File -replace “\\”, “/”
+    bash -c “vim $File”  
+}
+
+
+function nano($File){
+    $File = $File -replace “\\”, “/”
+    bash -c “nano $File”
+}
+
+function touch($File){
+    New-Item $File -ItemType File
+}
---
+++ Sync-Powershell.ps1
@@ -0,0 +1,13 @@
+﻿function Sync-Powershell([string]$message)
+{
+    cd 'C:\Users\bgreenawald\Documents\WindowsPowerShell\Scripts\autoload\General';
+
+    # Sync with remote
+    git pull
+
+    # Push any changes to remote
+    git add .
+    git commit -m $message
+    git push
+}
+
